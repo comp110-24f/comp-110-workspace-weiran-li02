119cf8f800764d5046473e89a7563de48f82a846
weiran-li02 <liweiran@ad.unc.edu>
2024-08-24 21:41:24 -0400

    first version

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,13 @@
+"""My first exercise in COMP110!"""
+
+__author__ = "730799401"
+
+print("Hello,world!")
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
+
+
+greet(name="Campers")
